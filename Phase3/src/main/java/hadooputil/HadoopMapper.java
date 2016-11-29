c4c043b494af70317d29fc64f68aea3e45e5e5be
thamizharasan90 <thamizharasan90@gmail.com>
2016-11-29 00:20:48 -0700

    map reduce v2

--- Phase3/src/main/java/hadooputil/HadoopMapper.java
+++ Phase3/src/main/java/hadooputil/HadoopMapper.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.Map.Entry;
-//import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -19,64 +18,51 @@ public class HadoopMapper extends Mapper<LongWritable, Text, LongWritable, Text>
 	@Override
 	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, LongWritable, Text>.Context context) throws IOException, InterruptedException 
 	{
-		//BoundaryHelper bh = bh.bound;
-		//DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		//System.out.println("**********INSIDE MAPPER*********");
         String inputLine = value.toString();
-        
         String[] stringArray = inputLine.split("\\,");
-        //Text outputText = new Text();
-        //System.out.println("output from mapper "+  stringArray.toString());
-        
+        //System.out.println("Length of input line :  " + stringArray.length);
 		if (true) 
 		{
 			String dateStr = (stringArray[1].split(" "))[0];
 			String longStr = stringArray[5];
 			String latStr = stringArray[6];
-			//System.out.println(dateStr);
-			//System.out.println(longStr);
-			//System.out.println(latStr);
-
 			try 
 			{	
-				//System.out.println("first line try " );
 				double longt = Double.parseDouble(longStr);
 				double latt = Double.parseDouble(latStr);
-				//System.out.println("longt longt" + longt +"" + latt);
 				int x = bh.getXCell(longt);
 				int y = bh.getYCell(latt);
 				int t = bh.getDCell(dateStr);
-				//System.out.println("try " + x + y + t);
 
 				if (x > -1 && y > -1 && t > -1 && x <= bh.xnoCell && y <= bh.ynoCell && t <= 30) 
 				{	
-					//System.out.println("if loop ");
 					SpaceIndexObj spaceCell = new SpaceIndexObj(x, y, t);
 					if (map.containsKey(spaceCell)) 
 					{
+						//System.out.println("inside if exsist part BEFORE:- " + spaceCell.x + "  " + spaceCell.y + "  " + spaceCell.t );
 						map.get(spaceCell).incrementAndGet();
-						System.out.println("inside try catch" + spaceCell.t + spaceCell.x + spaceCell.y);
+						//System.out.println("inside if exsist part AFTER INCREMENTANDGET:- " + spaceCell.x + "  " + spaceCell.y + "  " + spaceCell.t );
 					} 
-					else
+					else 
 					{
+						//System.out.println("else not exsist part BEFORE :-" + spaceCell.x + "  " + spaceCell.y + "  " + spaceCell.t );
 						map.put(spaceCell, new AtomicInteger(1));
-						System.out.println("else loop " + spaceCell.t + spaceCell.x + spaceCell.y );
+						//System.out.println("else not exsist part AFTER :-" + spaceCell.x + "  " + spaceCell.y + "  " + spaceCell.t );
 					}
 				}
 			} 
 			catch (Exception e) 
 			{	
 				System.out.println("catch output " + e);;
-				// do nothing
-				
 			}
 		}
 		
 		for (Entry<SpaceIndexObj, AtomicInteger> entry : map.entrySet()) 
 		{
 			SpaceIndexObj spaceCell = entry.getKey();
-			System.out.println("X:" + spaceCell.x + ",Y:" +  spaceCell.y + ",D:" + spaceCell.t + ",Value:" + entry.getValue().intValue());
+			//System.out.println("X: " + spaceCell.x + ",Y: " +  spaceCell.y + ",D: " + spaceCell.t + ",Value: " + entry.getValue().intValue());
 			long ltemp=Long.parseLong(Integer.toString(spaceCell.x) + Integer.toString(spaceCell.y) + Integer.toString(spaceCell.t), 10);
-		//	outputText.set("" + spaceCell.x + "," + spaceCell.y + "," + spaceCell.t + "," + entry.getValue().intValue());
 			context.write(new LongWritable(ltemp), new Text("" + spaceCell.x + "," + spaceCell.y + "," + spaceCell.t + "," + entry.getValue().intValue()));
 		}
 	}
--- Phase3/src/main/java/hadooputil/HadoopReducer.java
+++ Phase3/src/main/java/hadooputil/HadoopReducer.java
@@ -21,16 +21,18 @@ public class HadoopReducer extends Reducer<LongWritable, Text, Text, LongWritabl
 	protected void reduce(LongWritable arg0, Iterable<Text> arg1,
 			Reducer<LongWritable, Text, Text, LongWritable>.Context arg2) throws IOException, InterruptedException 
 	{
+		//System.out.println("**********INSIDE REDUCER*********");
 		HashMap<SpaceIndexObj, AtomicInteger> mp = new HashMap<>();
 		
 		for(Text val : arg1)
 		{
 			String[] splitStr = val.toString().split("\\,");
+			//System.out.println("Length of input line :  " + splitStr.length);
 			int x = Integer.parseInt(splitStr[0]);
 			int y = Integer.parseInt(splitStr[1]);
 			int d = Integer.parseInt(splitStr[2]);
 			int v = Integer.parseInt(splitStr[3]);
-			
+			//System.out.println("FOR POINT :  X: " + x + " Y: " + y + " D: " + d + " V: "+ v);
 			for (int i = -1; i <= +1; ++i) 
 			{
 				for (int j = -1; j <= +1; ++j) 
@@ -38,18 +40,30 @@ public class HadoopReducer extends Reducer<LongWritable, Text, Text, LongWritabl
 					for (int k = -1; k <= +1; ++k) 
 					{
 						SpaceIndexObj stc = new SpaceIndexObj(x + i, y + j, d + k + 1);
-						if (!mp.containsKey(stc)) {
+						if (!mp.containsKey(stc)) 
+						{
+							//System.out.println("inside not exsist part BEFORE:- " + stc.x + "  " + stc.y + "  " + stc.t + "  " + v );
 							mp.put(stc, new AtomicInteger(v));
-						} else {
+							//System.out.println("inside not exsist part AFTER:- " + stc.x + "  " + stc.y + "  " + stc.t+ "  " + mp.get(stc));
+						} 
+						else 
+						{
+							//System.out.println("inside if exsist part BEFORE:- " + stc.x + "  " +  stc.y + "  " + stc.t + "  " + mp.get(stc));
 							mp.get(stc).addAndGet(v);
+							//System.out.println("inside if exsist part AFTER ADDANDGET:- " + stc.x + "  " +  stc.y + "  " + stc.t + "  " + mp.get(stc));
+
 						}
 					}
 				}
 			}
 		}
+		
 		LinkedList<SpatialTuple> list = new LinkedList<>();
-		for (Entry<SpaceIndexObj, AtomicInteger> entry : mp.entrySet()) {
+		
+		for (Entry<SpaceIndexObj, AtomicInteger> entry : mp.entrySet()) 
+		{
 			SpaceIndexObj stc = entry.getKey();
+			//System.out.println("X: " + stc.x + ",Y: " +  stc.y + ",D: " + stc.t + ",Value: " + entry.getValue().intValue());
 			SpatialTuple stvt = new SpatialTuple(stc.x, stc.y, stc.t, entry.getValue().get());
 			list.add(stvt);
 		}
@@ -62,8 +76,8 @@ public class HadoopReducer extends Reducer<LongWritable, Text, Text, LongWritabl
 		for (int i = 0; i < Math.min(array.length, 50); ++i) 
 		{
 			outputText.set(array[i].toString());
-			System.out.println("reducer value " + outputText);
 			arg2.write(outputText, null);
+			//System.out.println("Reducer value : " + outputText);
 		}
 		// TODO Auto-generated method stub
 		//super.reduce(arg0, arg1, arg2);
